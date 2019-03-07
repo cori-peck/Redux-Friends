b79f091bff346b1de8bf80cb64a465cb9bbac0f9
cori-peck <cori.peck@ymail.com>
2019-03-07 17:48:44 -0600

    built route and component with a form for login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Login from './components/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
